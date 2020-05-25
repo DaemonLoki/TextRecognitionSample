cd2db3ee4e88b9afb978aa47019ef6967624282d
Stefan Blos <stefan.blos@codecamp-n.com>
2020-05-25 22:28:49 +0200

    Initial Commit

---
+++ Text Recognition Sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Text Recognition Sample
+//
+//  Created by Stefan Blos on 25.05.20.
+//  Copyright © 2020 Stefan Blos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
